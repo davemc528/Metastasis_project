a7b60bba76ac05fc5c6a804161a2ba4adaacf446
davemc528 <dcunnin1@tulane.edu>
2017-11-21 13:46:38 -0600

    Did fold change analysis, played around with p/s (no differences), changed the K-M curve as there was an erroneous data point.

--- half_million_cell_experiment/kaplan_meier_analysis.R
+++ half_million_cell_experiment/kaplan_meier_analysis.R
@@ -1,5 +1,10 @@
 #constructing kaplan-meier curve; need to play around with the aesthetics
 kaplan_meier_data <- read_csv("~/Box/Metastasis_project/Metastasis_project_data_analysis/R_file/half_million_cell_experiment/kaplan_meier_data.csv")
-sfit <- survfit(Surv(survival.time,status)~group,data = kaplan_meier_data)
+#install ggplot2 as well
+library("survival", lib.loc="~/Library/R/3.3/library")
+library("survminer", lib.loc="~/Library/R/3.3/library")
+library("ggfortify", lib.loc="~/Library/R/3.3/library")
+sfit <- surv_fit(Surv(survival.time,status)~group,data = kaplan_meier_data)
 library("survminer", lib.loc="~/Library/R/3.3/library")
 ggsurvplot(sfit)
+survdiff(Surv(survival.time)~group,data = kaplan_meier_data)
